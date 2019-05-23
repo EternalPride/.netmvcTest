c39c923f8bec566a1d57eea783fdf0d9fcdb7dc8
Dangerous Creatures <hr494@outlook.com>
2019-05-23 11:40:01 +0800

    mvcTest
    .netMVC测试

---
+++ hr/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hr.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            Models.Person pe = new Models.Person();
+            List<Models.Person> list = new List<Models.Person>();
+            pe.Age = 50;
+            pe.Name = "浩然";
+            list.Add(pe);
+            return View(list);
+        }
+	}
+}
---
+++ hr/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hr.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //public Person() 
+        //{
+        //    Name = "浩然";
+        //    Age = 20;
+        //}
+    }
+}
