05a72429d4f8ae07aced0f81f32f51cf55fc8244
EternalPride <hr494@outlook.com>
2019-05-30 12:11:26 +0800

    强类型

---
+++ hr/Controllers/SrtongController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hr.Controllers
+{
+    public class SrtongController : Controller
+    {
+        //
+        // GET: /Srtong/
+        Models.OTSEntities db = new Models.OTSEntities();
+        public ActionResult Index()
+        {
+            var q = db.Achievement.ToList();
+            return View(q);
+        }
+        public ActionResult Creat()
+        {
+            return View();
+        }
+        public ActionResult Creat(Models.Achievement model) 
+        {
+            db.Achievement.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+	}
+}
