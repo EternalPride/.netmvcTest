bd6a92c7d9c4ad16d705b4c213b0a1abcda3ee9a
EternalPride <hr494@outlook.com>
2019-05-31 11:13:35 +0800

    Method附加下拉框内容

---
+++ hr/Controllers/MethodController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hr.Controllers
+{
+    public class MethodController : Controller
+    {
+        //
+        // GET: /Method/
+        Models.OTSEntities db = new Models.OTSEntities();
+        public ActionResult Index()
+        {
+            var q = db.Achievement.ToList();
+            return View(q);
+        }
+        public ActionResult Edit(int id)
+        {
+            var q = db.Achievement.Where(s => s.ID == id).FirstOrDefault();
+            var n = db.Achievement.ToList();
+            SelectList list = new SelectList(n, "WoType", "WoType", q.WoType);
+            ViewBag.list = list;
+            return View(q);
+        }
+	}
+}
