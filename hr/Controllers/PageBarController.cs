490eefb77528c0a5eb115b3457c0c765f919f4bd
EternalPride <hr494@outlook.com>
2019-06-14 11:34:32 +0800

    无限分页

---
+++ hr/Controllers/PageBarController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hr.Controllers
+{
+    public class PageBarController : Controller
+    {
+        //
+        // GET: /PageBar/
+        Models.StudentEntities ghr = new Models.StudentEntities();
+        public ActionResult Index()
+        {   int PI=Request["PI"] !=null ? Int32.Parse(Request["PI"]) :1;
+            ViewBag.PI = PI;
+            var q = ghr.Student.ToList();
+            return View(q);
+        }
+	}
+}
---
+++ hr/Models/PageBar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+
+namespace hr.Models
+{
+    public class PageBar
+    {    
+        /// <summary>
+        /// 无限分页
+        /// </summary>
+        /// <param name="PI">当前页</param>
+        /// <param name="PC">数据总页</param>
+        /// <returns></returns>
+        public static string GetpageBar(int PI, int PC) 
+        {
+            if (PC == 1)
+                return null;
+            int start = PI - 5;
+            if (start < 1)
+                start = 1;
+            int end = start + 9;
+            if (end > PC)
+                end = PC;
+            StringBuilder hr = new StringBuilder();
+            if (PI > 1)
+                hr.Append(string.Format("<a href='?PI={0}'>上一页</a>", PI - 1));
+            for (int i = start; i <= end; i++) 
+            {
+                if (i == PI)
+                    hr.Append(i);
+                else
+                    hr.Append(string.Format("<a href='?PI={0}'>{0}</a>", i));
+               
+            } if (PI < PC) { hr.Append(string.Format("<a href='?PI={0}'>下一页</a>", PI +1)); } 
+            return hr.ToString();
+        }
+    }
+}
